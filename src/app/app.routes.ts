e4a2d138cdca62da95a065616dc75ca5fe0bda40
ahmedfourati11 <ahmedfourati44@gmail.com>
2024-12-04 20:02:01 +0100

    "First Commit"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:HeroSectionComponent}
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,15 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        blueCustom: "#004AAD",
+        orangeCustom: "#FF6F00",
+        grayLight: "#F4F4F4",
+        customBlue: '#003F7D',
+      },
+    },
+  },
 }
 
