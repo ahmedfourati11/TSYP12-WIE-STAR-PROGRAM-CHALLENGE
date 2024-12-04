45e19b9b9843834c6e0b8f9e91e42c2b44b08bc8
ahmedfourati11 <ahmedfourati44@gmail.com>
2024-12-04 21:33:18 +0100

    "Sign_up_Page_Commit"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
-    {path:"",component:HeroSectionComponent}
+    {path:"",component:HeroSectionComponent},
+    {path:"Sign_Up",component:SignUpComponent},
 ];
