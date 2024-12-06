d1a3875b5bdd58c0f52abd9c0c26c6ad380b9e15
ahmedfourati11 <ahmedfourati44@gmail.com>
2024-12-06 19:41:16 +0100

    "Navbar_Test_Commit"

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }
