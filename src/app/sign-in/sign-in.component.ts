f3312a02349ba32d02aa7da1cae98a1fcee918b7
ahmedfourati11 <ahmedfourati44@gmail.com>
2024-12-08 19:56:09 +0100

    "Fixed_Sign_In_Page"

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+
+  email = '';
+  password = '';
+  rememberMe = false;
+
+  constructor(private router:Router) {}
+
+  onLogin() {
+    
+    if (!this.email || !this.password) {
+      alert('Please fill all fields!');
+      return;
+    }
+
+    const firstLetter = this.password[0].toLowerCase();
+    if (firstLetter === 's') {
+      this.router.navigate(['/Student_Home']);
+    } else if (firstLetter === 'a') {
+      this.router.navigate(['/Adult_Home']);
+    } else {
+      alert('Invalid password.');
+    }
+  }
 
 }
