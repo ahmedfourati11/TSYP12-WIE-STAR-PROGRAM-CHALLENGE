029c871f78db5b188f836c7c2e7e7cc3c85054b9
ahmedfourati11 <ahmedfourati44@gmail.com>
2024-12-08 21:46:08 +0100

    "Page_Contact_Fixed"

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
@@ -15,5 +16,37 @@ export class ContactComponent {
   toggleSwitch(): void {
     this.isChecked = !this.isChecked;  
   }
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+    category: '',
+    message: ''
+  };
 
+  isModalVisible = false;
+
+  onSubmit() {
+    if (
+      this.formData.name &&
+      this.formData.email &&
+      this.formData.phone &&
+      this.formData.category &&
+      this.formData.message
+    ) {
+      // Affiche la modale si le formulaire est valide
+      this.isModalVisible = true;
+    }
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
+    this.formData = {
+      name: '',
+      email: '',
+      phone: '',
+      category: '',
+      message: ''
+    };
+  }
 }
