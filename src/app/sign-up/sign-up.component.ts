1b47d22d118d4debefec7db68191914737749a61
ahmedfourati11 <ahmedfourati44@gmail.com>
2024-12-08 19:31:29 +0100

    "Sign_Up_Fixed_Page"

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,13 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule,CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  firstName = '';
+  lastName = '';
+  email = '';
+  userType: 'Student' | 'Adult' | '' = '';
+  password = '';
+  confirmPassword = '';
+  rememberMe = false;
+  showModal = false;
 
+  constructor(private router:Router) {}
+
+  onSubmit() {
+    if (this.firstName && this.lastName && this.email && this.password && this.confirmPassword && this.userType) {
+      if (this.password === this.confirmPassword) {
+        this.showModal = true; 
+      } else {
+        alert('Passwords do not match!');
+      }
+    } else {
+      alert('Please fill all the fields!');
+    }
+  }
+
+  goToSignIn() {
+    this.showModal = false; 
+    this.router.navigate(['/Sign_In']);
+  }
 }
